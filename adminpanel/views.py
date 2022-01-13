1410bf75fe0dfa37c14bb055640f78958debc8a4
Akhil mohanan <akhilmk444@gmail.com>
2022-01-13 18:49:53 +0530

    report

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -30,8 +30,8 @@ def adminlogin(request):
         yearReports = yearReport(request)
         pendingorder = pendingorders(request)
         deliveredorder = deliveredorders(request)
-        report_month = order.objects.values('date__month','date__day','date__year').filter(status='Delivered').annotate(Sum('total')).order_by('date__date')[:7]
-        report_date = order.objects.values('date__month','date__day','date__year').filter(status='Delivered').annotate(Sum('total')).order_by('date__date')[:7]
+        report_month = order.objects.values('date__month','date__day','date__year').filter(status='Delivered').annotate(Sum('total')).order_by('date__date')[:9]
+        report_date = order.objects.values('date__month','date__day','date__year').filter(status='Delivered').annotate(Sum('total')).order_by('date__date')[:9]
         payment_mode = order.objects.values('payment_mode').filter(status='Delivered').annotate(Sum('total')).all()
         order_status = order.objects.values('status').annotate(Count('status'))
         return render(request,'adminindex.html',{'total':report_month,'report_date':report_date,'payment':payment_mode,'order_status':order_status,'monthReports':monthReports,'yearReports':yearReports,'pendingorder':pendingorder,'deliveredorder':deliveredorder})
